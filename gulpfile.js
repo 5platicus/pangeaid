3e83e60dfd5577a670abc70185769b79a4f3a139
SamZamor <sam@kweaky.com>
2019-05-14 13:17:30 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var gulpConnect = require('gulp-connect');
+var gulpLess = require('gulp-less');
+var gulpEjs = require('gulp-ejs');
+var src = 'src';
+
+var out = 'public';
+
+gulp.task("connect", function () {
+  return gulpConnect.server({
+    root: "public",
+    port: 1337,
+    livereload: true
+  });
+});
+
+gulp.task('ejs', function () {
+  return gulp.src([src + '/*.ejs'])
+    .pipe(gulpEjs())
+    .pipe(gulp.dest(out + '/'))
+    .pipe(gulpConnect.reload());
+});
+
+gulp.task('less', function () {
+  return gulp.src('./src/less/**/*.less')
+    .pipe(gulpLess())
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(gulpConnect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch([src + '/less/*.less', src + '/less/**/*.less'], ['less']);
+  return gulp.watch([src + '/*.ejs', src + '/ejs/**/*.ejs'], ['ejs']);
+});
+
+gulp.task('default', ['connect', 'less', 'ejs', 'watch']);
