801c7d3130915715acd6c9dca26c1ec9270ca880
Ivan Vukusic <ivanvukusic15@gmail.com>
2019-05-15 22:31:52 +0200

    Pasting profile action

--- public/js/Main.js
+++ public/js/Main.js
@@ -15,12 +15,12 @@ let availableTabs = {
         icon: 'fa-wallet',
     },
 };
-$(".tabLink").on('click', function() {
+$('.tabLink').on('click', function() {
     let tab = $(this).data("id");
     $(".tabLink").removeClass("current");
     $(this).addClass("current");
     
-    jQuery.each(availableTabs, function(key, val) {
+    $.each(availableTabs, function(key, val) {
         if (val.id === tab) {
             $(val.id).show();
             $(val.id).css('display', 'inline-flex');
@@ -31,3 +31,25 @@ $(".tabLink").on('click', function() {
         }
     });
 });
+
+$('#pasteProfile').on('click', function() {
+    let variables = {
+        surname: 'Miora',
+        firstName: 'Rakotomalala',
+        birthDate: '1980-05-18',
+        birthPlace: 'Madagascar',
+        nationality: 'Malagasy',
+        passportNumber: '07D703249',
+        dateIssued: '2013-07-15',
+        placeIssued: 'Antananarivo',
+        validUntil: '2023-05-18',
+        issuingAuthority: 'Madagascar',
+    };
+    $.each(variables, function(key, val) {
+        $('[data-profile="' + key + '"]').val(val);
+        $('[data-profile="' + key + '"]').css('border', '1px solid #6980b5');
+    })
+    $('[data-profile="profilePic"]').text('');
+    $('[data-profile="profilePic"]').css('background', 'url(../images/profilePic.png)');
+    $('[data-profile="profilePic"]').css('background-size', 'cover');
+})
