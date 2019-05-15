ecd85f52b665ad8f4e7708d877e009e4ac67703d
Ivan Vukusic <ivanvukusic15@gmail.com>
2019-05-15 21:49:08 +0200

    Header changes and modal closing

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,20 +1,33 @@
 let availableTabs = {
-    govLink: '#gov',
-    docsLink: '#docs',
-    walletLink: '#wallet',
+    govLink: {
+        id: '#gov',
+        title: 'MY GOV',
+        icon: 'fa-landmark',
+    },
+    docsLink: {
+        id: '#docs',
+        title: 'MY DOCS',
+        icon: 'fa-id-card',
+    },
+    walletLink: {
+        id: '#wallet',
+        title: 'MY WALLET',
+        icon: 'fa-wallet',
+    },
 };
-
 $(".tabLink").on('click', function() {
     let tab = $(this).data("id");
     $(".tabLink").removeClass("current");
     $(this).addClass("current");
     
     jQuery.each(availableTabs, function(key, val) {
-        if (val === tab) {
-            $(val).show();
-            $(val).css('display', 'inline-flex');
+        if (val.id === tab) {
+            $(val.id).show();
+            $(val.id).css('display', 'inline-flex');
+            $('#screenTitle').text(val.title);
+            $("#screenIcon").attr('class', 'fa ' + val.icon);
         } else {
-            $(val).hide();
+            $(val.id).hide();
         }
     });
 });
--- public/js/animatedModal.js
+++ public/js/animatedModal.js
@@ -33,9 +33,6 @@
             afterOpen: function() {}, 
             beforeClose: function() {}, 
             afterClose: function() {}
- 
-            
-
         }, options);
         
         var closeBt = $('.close-'+settings.modalTarget);
@@ -118,6 +115,56 @@
         }
 
     }; // End animatedModal.js
+
+    $.fn.close = function(options) {
+        var settings = $.extend({
+            modalTarget: modal.attr('href').replace('#',''), 
+            // position:'fixed', 
+            // width:'100%', 
+            height:'100%', 
+            // top:'0px', 
+            // left:'0px', 
+            zIndexIn: '9999',  
+            zIndexOut: '-9999',  
+            //color: '#0B1459', 
+            opacityIn:'1',  
+            opacityOut:'0', 
+            animatedIn:'fadeInUpBig',
+            animatedOut:'fadeOutDownBig',
+            animationDuration:'.6s', 
+            overflow:'auto', 
+            // Callbacks
+            beforeOpen: function() {},           
+            afterOpen: function() {}, 
+            beforeClose: function() {}, 
+            afterClose: function() {}
+        }, options);
+        
+        var closeBt = $('.close-'+settings.modalTarget);
+    
+        closeBt.click(function(event) {
+            event.preventDefault();
+            $('body, html').css({'overflow':'auto'});
+
+            settings.beforeClose(); //beforeClose
+            if (id.hasClass(settings.modalTarget+'-on')) {
+                id.removeClass(settings.modalTarget+'-on');
+                id.addClass(settings.modalTarget+'-off');
+            } 
+
+            if (id.hasClass(settings.modalTarget+'-off')) {
+                id.removeClass(settings.animatedIn);
+                id.addClass(settings.animatedOut);
+                id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterClose);
+            };
+
+        });
+
+        function afterClose () {       
+            id.css({'z-index':settings.zIndexOut});
+            settings.afterClose(); //afterClose
+        }
+    }
 
 }(jQuery));
 
