5f06b0d61987912c59d4ebbf8dd95a3278a714ba
Ivan Vukusic <ivanvukusic15@gmail.com>
2019-05-18 22:25:30 +0200

    Successfull payment alert

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,3 +1,4 @@
+let successAnimation = "<svg id='successAnimation' class='animated' xmlns='http://www.w3.org/2000/svg' width='70' height='70' viewBox='0 0 70 70'><path id='successAnimationResult' fill='#D8D8D8' d='M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z'/><circle id='successAnimationCircle' cx='35' cy='35' r='24' stroke='#979797' stroke-width='2' stroke-linecap='round' fill='transparent'/><polyline id='successAnimationCheck' stroke='#979797' stroke-width='2' points='23 34 34 43 47 27' fill='transparent'/></svg>";
 
 $(document).ready(function() {
     initializeModals();
@@ -35,13 +36,27 @@ $(document).ready(function() {
     */
     $('.back-visaApplication').on('click', function() {
         resetProfile();
-    })
+    });
     $('.back-prePayment').on('click', function() {
         resetIdentityValidation();
-    })
+    });
     $('.back-paymentValidation').on('click', function() {
         resetSignature();
-    })
+    });
+    $('#displayQREmptyForm').on('click', function() {
+        setTimeout(function() {
+            showAlertSuccess( "Successful transaction",  "Successful transaction");
+        }, 10000)
+    });
+    $('#sendPaymentValidationFormEmpty').on('click', function() {
+        showAlertSuccess( "Successful transaction",  "Successful transaction");
+    });
+    // $('#paymentValidationFormEmpty').on('click', function() {
+    //     showAlertSuccess( "Successful transaction",  "Successful transaction");
+    // });
+    // $('#payPaymentValidationFormEmpty').on('click', function() {
+    //     showAlertSuccess( "Successful transaction",  "Successful transaction");
+    // });
 });
 
 /**
@@ -63,7 +78,6 @@ function initializeModals() {
     $("#travelCategoryFormEmpty").animatedModal(options);
     $("#visaApplicationFormEmpty").animatedModal(options);
     $("#prePaymentFormEmpty").animatedModal(options);
-    $("#paymentValidationFormEmpty").animatedModal(options);
 
     $("#docsFormEmpty").animatedModal();
 
@@ -73,13 +87,9 @@ function initializeModals() {
     $("#sendPaymentMethodFormEmpty").animatedModal(options);
     $("#sendRecipientInformationFormEmpty").animatedModal(options);
     $("#sendAuthenticationFormEmpty").animatedModal(options);
-    $("#sendPaymentValidationFormEmpty").animatedModal(options);
-    $("#sendSuccessFormEmpty").animatedModal(options);
     $("#payPaymentMethodFormEmpty").animatedModal(options);
     $("#payRecipientInformationFormEmpty").animatedModal(options);
     $("#payAuthenticationFormEmpty").animatedModal(options);
-    $("#payPaymentValidationFormEmpty").animatedModal(options);
-    $("#paySuccessFormEmpty").animatedModal(options);
 }
 
 /**
@@ -138,11 +148,11 @@ function tabNavigation() {
                 $('.nav').attr('class', 'nav selected ' + currentNav);
                 setTimeout(function () {
                     $('.nav').attr('class', 'nav selected ' + val.nav);
-                }, 200) 
+                }, 100) 
                 setTimeout(function () {
                     $('.nav').attr('class', 'nav ' + val.nav);
                     currentNav = val.nav;
-                }, 400);
+                }, 200);
                 
                 $('#screenTitle').text(val.title);
                 $("#screenIcon").attr('class', 'fa ' + val.icon);
@@ -220,5 +230,14 @@ function resetSignature() {
 }
 function resetIdentityValidation(){
     $('#paymentValidationFormEmpty').attr('class', 'containerAction disabledAction')
-    $('#sendAuthenticationFormEmpty').attr('class', 'containerAction disabledAction')
+    $('#sendPaymentValidationFormEmpty').attr('class', 'containerAction disabledAction')
 };
+
+function showAlertSuccess(title, text) {
+    swal({
+        title: title,
+        text: text
+    });
+    $('<div class="swal-success-container"></div>').insertAfter('.swal-text');
+    $('.swal-success-container').html(successAnimation);
+}
